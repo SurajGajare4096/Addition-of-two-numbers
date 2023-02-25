6e2e85207677c1b31c6bf5eb9fa58a2ea8b84155
SurajGajare4096 <126334881+SurajGajare4096@users.noreply.github.com>
2023-02-25 19:25:29 +0530

    Create taking_input.py

---
+++ taking_input.py
@@ -0,0 +1,10 @@
+a = int(input("enter the 1st number :"))
+b = int(input("enter the 2nd number :"))
+c = a + b 
+d = a - b
+e = a * b
+f = a // b
+print("addition is : ",c)
+print("subtraction is : ",d)
+print("multiplication is : ",e)
+print("division is : ",f)
